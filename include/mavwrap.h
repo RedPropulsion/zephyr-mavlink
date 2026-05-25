329399044f3d45f135733158ac6d7e9b326cea5a
TheRealLorenz <lollobellina@gmail.com>
2026-05-25 15:55:02 +0200

    feat: support custom dialects

--- include/mavwrap.h
+++ include/mavwrap.h
@@ -10,17 +10,11 @@
 #ifndef MAVWRAP_H
 #define MAVWRAP_H
 
-#include <zephyr/device.h>
 #include <stddef.h>
 #include <stdint.h>
+#include <zephyr/device.h>
 
-#if defined(CONFIG_MAVWRAP_DIALECT_ARDUPILOTMEGA)
-#include <ardupilotmega/mavlink.h>
-#elif defined(CONFIG_MAVWRAP_DIALECT_COMMON)
-#include <common/mavlink.h>
-#else
-#error "No MAVLink dialect selected. Enable MAVWRAP and set MAVWRAP_DIALECT."
-#endif
+#include <mavlink_dialect.h>
 
 #ifdef __cplusplus
 extern "C" {
